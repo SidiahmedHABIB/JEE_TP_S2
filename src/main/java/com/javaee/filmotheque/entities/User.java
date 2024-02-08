84f2cf9ba98fdcf6fdcd637b00057d155d249806
sidi ahmed <sidiahmedhabib@gmail.com>
2024-02-08 16:13:20 +0100

    commit tp3

---
+++ src/main/java/com/javaee/filmotheque/entities/User.java
@@ -0,0 +1,2 @@
+package com.javaee.filmotheque.entities;public class User {
+}
---
+++ src/main/java/com/javaee/filmotheque/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.javaee.filmotheque.repositories;
+
+import com.javaee.filmotheque.entities.Acteur;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActeurRepository extends JpaRepository<Acteur,Long> {
+
+}
